6d35cf19b9f540f7b525a31e686688e3a593d858
Marko Knöbl <marko.kn@gmail.com>
2015-10-22 15:56:27 +0200

    provide defaults for "topic" and "language" if these are not specified in the URL

--- website.js
+++ website.js
@@ -4,11 +4,16 @@ angular.module('todoApp', ['ngSanitize', 'ngRoute'])
       templateUrl: function(params) {return 'templates/' + params.topic + '_' + params.lang + '.html';},
       controller: 'WebsiteController'
     });
+    $routeProvider.otherwise({redirectTo: '/aboutme/en'});
   })
   .controller('WebsiteController', function($scope, $location) {
     var parts = $location.path().split('/');
     this.topic = parts[1];
     this.language = parts[2];
+    if (!this.topic) {
+      this.topic = 'aboutme';
+      this.language = 'en';
+    }
     this.langs = {
       'en': 'English',
       'de': 'Deutsch'
