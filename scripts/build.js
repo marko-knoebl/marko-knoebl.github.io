26704fc25a1515dd12aaf0cb9fe915dad4cbd87a
Marko Knoebl <marko.kn@gmail.com>
2019-12-25 10:18:39 +0100

    building resume page from npm package '@karuga/karuga-jsonresume'

--- scripts/build.js
+++ scripts/build.js
@@ -2,15 +2,23 @@ const fs = require("fs");
 const fsExtra = require("fs-extra");
 
 const template = fs.readFileSync("template.html", "utf-8");
-console.log(template);
 
 const pagenames = fs.readdirSync("pages");
-console.log(pagenames);
 
 for (let pagename of pagenames) {
   const pagecontent = fs.readFileSync(`pages/${pagename}`, "utf-8");
   const fullPage = template.replace("{{content}}", pagecontent);
   fs.writeFileSync(`dist/${pagename}`, fullPage);
+}
+
+// create resume pages
+for (let lang of ["de", "en"]) {
+  const pagecontent = fs.readFileSync(
+    `node_modules/@karuga/karuga-jsonresume/dist/resume_react_bootstrap_material_${lang}.html`,
+    { encoding: "utf-8" }
+  );
+  const fullPage = template.replace("{{content}}", pagecontent);
+  fs.writeFileSync(`dist/cv_${lang}.html`, fullPage);
 }
 
 fsExtra.copySync("images", "dist/images");
